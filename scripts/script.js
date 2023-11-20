80553c87605e8fa01ced8d8a8949255c9a36014d
KvJellyBean <kevinnatan91@gmail.com>
2023-11-20 13:44:27 +0700

    Add event handler for range input of grid size
    Add a change event handler where when it's value change, auto update the size text and grid workstation

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ let gridSizeText = document.querySelector('label#gridText')
 const workstation = document.querySelector('.container');
 
 function createGrid(gridSize) {
+    workstation.innerHTML = '';
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             const div = document.createElement('div');
@@ -11,8 +12,24 @@ function createGrid(gridSize) {
     }
 }
 
+/* Event handler for page when DOM fully load 
+    Output the value of grid size from range input
+    Create the grid into workstation*/
 document.addEventListener('DOMContentLoaded', () => {
     const rangeValue = gridRange.value;
     gridSizeText.innerText = `Grid Size: ${rangeValue} x ${rangeValue}`;
+
+    createGrid(rangeValue);
+});
+
+// Event handler for range input when it's value changed
+gridRange.addEventListener('change', () => {
+    const rangeValue = gridRange.value;
+
+    workstation.style.gridTemplateRows = `repeat(${rangeValue}, 1fr)`;
+    workstation.style.gridTemplateColumns = `repeat(${rangeValue}, 1fr)`;
+
+    gridSizeText.innerText = `Grid Size: ${rangeValue} x ${rangeValue}`;
+
     createGrid(rangeValue);
 });
